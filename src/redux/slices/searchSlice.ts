7fe617035e3bec0ab7aaebbaf333d374699f7afc
BrunoUA96 <ostapklysa@gmail.com>
2023-06-21 11:30:50 +0100

    add type to setSearchValue action in the searchSlice

--- src/redux/slices/searchSlice.ts
+++ src/redux/slices/searchSlice.ts
@@ -14,7 +14,7 @@ export const searchSlice = createSlice({
   name: "search",
   initialState,
   reducers: {
-    setSearchValue: (state, action) => {
+    setSearchValue: (state, action: PayloadAction<string>) => {
       state.searchValue = action.payload;
     },
   },
