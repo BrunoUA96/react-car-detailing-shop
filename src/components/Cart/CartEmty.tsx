b3b23db2a8b85ed1aa83ce346892935f0b3aa1d0
BrunoUA96 <ostapklysa@gmail.com>
2023-06-27 15:22:56 +0100

    responsive

--- src/components/Cart/CartEmty.tsx
+++ src/components/Cart/CartEmty.tsx
@@ -5,24 +5,22 @@ import emtyCartImg from "../../assets/empty-cart.png";
 
 const CartEmty: React.FC = () => {
   return (
-    <div className="content">
-      <div className="container container--cart">
-        <div className="cart cart--empty">
-          <h2>
-            Cart is empty <span>😕</span>
-          </h2>
-          <p>
-            You probably haven't added anything yet.
-            <br />
-            To add a product go to the home page.
-          </p>
+    <div className="container container--cart">
+      <div className="cart cart--empty">
+        <h2>
+          Cart is empty <span>😕</span>
+        </h2>
+        <p>
+          You probably haven't added anything yet.
+          <br />
+          To add a product go to the home page.
+        </p>
 
-          <img src={emtyCartImg} alt="Empty cart" />
+        <img src={emtyCartImg} alt="Empty cart" />
 
-          <Link to="/" className="button button--black">
-            <span>Home page</span>
-          </Link>
-        </div>
+        <Link to="/" className="button button--black">
+          <span>Home page</span>
+        </Link>
       </div>
     </div>
   );
